4abded8afafc946ed4c9791ddd0de57e06354227
Jessica Jones <daddehjones@gmail.com>
2021-05-18 19:39:05 -0400

    change color name

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -10,8 +10,16 @@ RSpec.describe Color do
 
     it 'can have a name' do
      color = Color.new("Red")
-     
+
      expect(color.name).to eq("Red")
-   end
- end
+    end
+  end
+
+  describe 'multiple colors' do
+    it 'can have another name' do
+     color = Color.new("Green")
+
+     expect(color.name).to eq("Green")
+    end
+  end
 end
