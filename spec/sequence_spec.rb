725efddc87e76f123d6fb8d5a03be2e1603f7148
Jessica Jones <daddehjones@gmail.com>
2021-05-18 20:29:33 -0400

    test #initialized

--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -1,1 +1,17 @@
-require './color_spec'
+require './lib/color'
+require './lib/sequence'
+
+RSpec.describe Sequence do
+  describe '#initialize' do
+    it 'exists' do
+      red = Color.new("Red")
+      green = Color.new("Green")
+      blue = Color.new("Blue")
+      yellow = Color.new("Yellow")
+
+      sequence = Sequence.new([red, green, blue, yellow])
+
+      expect(sequence).to be_an_instance_of(Sequence)
+    end
+  end
+end
