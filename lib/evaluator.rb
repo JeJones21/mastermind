9e0b380b781edc62c01e762c763cf84ff367a4d4
sami-p <sami.peterson14@gmail.com>
2021-05-22 13:49:30 -0500

    Add #placement method

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -9,4 +9,14 @@ class Evaluator
   def transform_guess(user_guess)
     @guess = user_guess.upcase.split(//)
   end
+
+  def placement
+    placement_counter = 0
+    @guess.length.times do |i|
+      if @guess[i] == @code.secret_code[i]
+        placement_counter += 1
+      end
+    end
+    return placement_counter
+  end
 end
