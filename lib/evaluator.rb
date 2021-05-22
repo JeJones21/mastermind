146da5dd6b5b3ff630c28e6a971d010532f350ce
sami-p <sami.peterson14@gmail.com>
2021-05-22 13:48:25 -0500

    Add #transform_guess method

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -1,7 +1,12 @@
 class Evaluator
-  attr_reader :code
-  
+  attr_reader :code,
+              :guess
+
   def initialize(code)
     @code = code
+  end
+
+  def transform_guess(user_guess)
+    @guess = user_guess.upcase.split(//)
   end
 end
