4e58af458efc895726201747558fa0d3765db8f7
sami-p <sami.peterson14@gmail.com>
2021-05-24 16:33:08 -0500

    Test that Game is initialized with Code

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe Game do
 
       expect(game).to be_an_instance_of(Game)
     end
+
+    it 'is initialized with a code' do
+      code = Code.new
+      game = Game.new
+
+      expect(game.code).to be_an_instance_of(Code)
+    end
   end
 end
