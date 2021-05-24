a5c9d3691713a01cb50c311ed8a09920173d7654
sami-p <sami.peterson14@gmail.com>
2021-05-24 18:24:21 -0500

    Add #start and #menu methods

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,24 @@ class Game
     @code = Code.new
     @evaluator = Evaluator.new(code)
   end
+
+  def menu
+    puts " "
+    puts "Welcome to MASTERMIND"
+    puts " "
+    puts "Would you like to (p)lay, read the (i)instructions, or (q)uit?"
+    print "> "
+    input = gets.chomp.downcase
+    if input == 'p' || input == 'play'
+      start_game
+    elsif input == 'i' || input == 'instructions'
+      instructions
+    elsif input == 'q' || input == 'quit'
+      quit_game
+    end
+  end
+
+  def start
+    menu
+  end
 end
