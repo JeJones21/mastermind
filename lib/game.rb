36399dc277993fbf2c3ecaeb2669715a029e9541
Jessica Jones <daddehjones@gmail.com>
2021-05-26 19:47:12 -0400

    add instructions message

--- lib/game.rb
+++ lib/game.rb
@@ -78,19 +78,8 @@ class Game
   end
 
   def instructions
-      puts " "
-      puts "---------------------------------Instructions---------------------------------"
-      puts "A secret code containing 4 colors will be generated, and it's your"
-      puts "job to guess what it is! It will include a random order of (r)ed, (b)lue,"
-      puts "(g)reen and (y)ellow, and can even contain repeated colors. Guess the correct"
-      puts "colors in the correct order to win. Your guess should be 4 consecutive letters"
-      puts "like this: rygb"
-      puts  "Good luck!"
-      puts "------------------------------------------------------------------------------"
-      puts " "
-      puts "Are you ready to (p)lay?"
-      print "> "
-      play_or_quit
+    @messages.instruct
+    play_or_quit
   end
 
   def quit
