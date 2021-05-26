54a285638d773063aaf444be530725794d09505a
Jessica Jones <daddehjones@gmail.com>
2021-05-26 19:44:18 -0400

    initialize messages class

---
+++ lib/messages.rb
@@ -0,0 +1,13 @@
+class Messages
+
+def initialize
+end
+
+def display_menu
+  puts " "
+  puts "Welcome to 🧠 MASTERMIND 🧠"
+  puts " "
+  puts "Would you like to (p)lay, read the (i)instructions, or (q)uit?"
+  print "> "
+ end
+end
